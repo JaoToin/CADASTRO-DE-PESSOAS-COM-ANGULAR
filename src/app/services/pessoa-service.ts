56311eb352bceacfc29c3676155ce557790d0fa0
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-04 21:33:50 -0300

    iniciando o serviço no Angular e o models

---
+++ src/app/services/pessoa-service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { Pessoa } from "../models/pessoa";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PessoaService {
+
+    private pessoas: Pessoa[] = []
+
+    adcionar(pessoa: Pessoa){
+        this.pessoas.push(pessoa)
+    }
+
+    listar(){
+        return this.pessoas
+    }
+}
